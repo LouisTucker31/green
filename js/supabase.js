f7c57e19c796ce862068fbf92f5463c566be21a7
Louis Tucker <louistucker@Louiss-MacBook-Pro.local>
2026-06-09 11:41:05 +0100

    Fix supabase client spelling error

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,7 @@
 // Green PWA | Supase Base
 
-const supabase = window.supabase = createClient(
+const supabase = window.supabase.createClient(
     'https://mpebxipzdvuedgqlfoic.supabase.co',
     'sb_publishable_7oMsA23tasUZtnJ5Jd1Vmg_H7oXWW_I'
 );
+window.supabase = supabase;
